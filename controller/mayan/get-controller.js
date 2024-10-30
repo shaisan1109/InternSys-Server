4ee7ca668bfd3b786cdf15ca5118610ea0f99e2a
Shaira Santos <shaisan1109@gmail.com>
2024-10-31 03:13:47 +0800

    Major updates - Changed npm run dev to npm start - More Mayan get routes - More user routes

--- controller/mayan/get-controller.js
+++ controller/mayan/get-controller.js
@@ -45,8 +45,9 @@ const mayanGetController = (app, options, done) => {
     });
 
     // GET all documents of a certain document type
-    app.get('/documents-of-type/:id', async (request, reply) => {
-        const response = await fetch(`http://ccscloud.dlsu.edu.ph:12707/api/v4/document_types/${request.params.id}/documents/`,
+    // NOTE: Type is a number
+    app.get('/documents/:type', async (request, reply) => {
+        const response = await fetch(`http://ccscloud.dlsu.edu.ph:12707/api/v4/document_types/${request.params.type}/documents/`,
             {
                 method: 'GET',
                 headers: {
@@ -58,8 +59,8 @@ const mayanGetController = (app, options, done) => {
         return reply.send(json);
     });
 
-    // GET all info on a document
-    app.get('/document/:id', async (request, reply) => {
+    // GET one document and its info
+    app.get('/document/:id/info', async (request, reply) => {
         const response = await fetch(`http://ccscloud.dlsu.edu.ph:12707/api/v4/documents/${request.params.id}/?_fields_only=document_type__id,document_type__label,file_latest__filename`,
             {
                 method: 'GET',
@@ -100,9 +101,24 @@ const mayanGetController = (app, options, done) => {
         return reply.send(json);
     });
 
+    // GET document download
+    app.get('/document/:id/download', async (request, reply) => {
+        const response = await fetch(`http://ccscloud.dlsu.edu.ph:12707/api/v4/documents/${request.params.id}/files/${request.params.id}/download`,
+            {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Token f03a27ed9a6e0f92e6b3fd60a9b25a6c64173b32'
+                }
+            }
+        );
+        const file = await response;
+        return reply.send(file);
+    });
+
     // GET page image
     app.get('/document/:id/page/:pageid', async (request, reply) => {
         const buffer = fs.readFileSync(`http://ccscloud.dlsu.edu.ph:12707/api/v4/documents/${request.params.id}/files/${request.params.id}/pages/${request.params.pageid}/image`);
+
         reply.type('image/png'); // if you don't set the content, the image would be downloaded by browser instead of viewed
         reply.send(buffer);
     });
--- controller/postgres/user-controller.js
+++ controller/postgres/user-controller.js
@@ -15,7 +15,7 @@ const userController = (app, options, done) => {
 
     // GET one user with email
     app.get("/user", async (request) => {
-        const { email, password } = request.query;
+        const { email } = request.query;
 
         const client = await app.pg.connect();
         const userResult = await client.query(`SELECT * FROM public.user WHERE email=${email}`);
@@ -23,6 +23,31 @@ const userController = (app, options, done) => {
         client.release();
 
         return userResult.rows;
+    });
+
+    // GET all user types
+    app.get('/user-types', async () => {
+        const client = await app.pg.connect();
+        const typesResult = await client.query(`SELECT * FROM public.user_roles`);
+
+        client.release();
+
+        return typesResult.rows;
+    });
+
+    // GET all users of one type
+    app.get('/user-type/:roleid', async (request) => {
+        const client = await app.pg.connect();
+        const usersResult = await client.query(
+            `SELECT u.systemid, u.dlsuid, u.email, u.lastname, u.firstname, u.middlename, ur.rolename 
+            FROM public.user u
+            INNER JOIN public.user_roles ur ON u.roleid=ur.roleid
+            WHERE u.roleid=${request.params.roleid}`
+        );
+
+        client.release();
+
+        return usersResult.rows;
     });
 
     done();
