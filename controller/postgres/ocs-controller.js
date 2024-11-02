8a2f6cd7503422669be6a93c6c385f7c73a3f6a7
Shaira Santos <shaisan1109@gmail.com>
2024-11-02 15:03:07 +0800

    Complete CRUD for job websites

--- controller/postgres/ocs-controller.js
+++ controller/postgres/ocs-controller.js
@@ -1,12 +1,53 @@
 const ocsController = (app, options, done) => {
-    // GET one user with email
-    app.get("/job-websites", async (request) => {
+    // GET all job websites
+    app.get("/job-websites", async () => {
         const client = await app.pg.connect();
         const sitesResult = await client.query(`SELECT * FROM public.job_websites`);
 
         client.release();
 
         return sitesResult.rows;
+    });
+
+    // CREATE a job website
+    app.post("/job-website/create", async (request) => {
+        const { title, desc, link } = request.body;
+        
+        return app.pg.transact(async (client) => {
+            const createLink = await client.query(`
+                INSERT INTO public.job_websites (title, description, link)
+                VALUES ('${title}', '${desc}', '${link}')
+            `);      
+            return createLink;
+        });
+    });
+
+    // UPDATE a job website
+    app.patch("/job-website/update/:id", async (request) => {
+        const { id } = request.params;
+        const { title, desc, link } = request.body;
+        
+        return app.pg.transact(async (client) => {
+            const updateLink = await client.query(`
+                UPDATE public.job_websites
+                SET title = '${title}', description = '${desc}', link = '${link}'
+                WHERE websiteid = ${id}
+            `);
+            return updateLink;
+        });
+    });
+
+    // DELETE a job website
+    app.delete("/job-website/delete/:id", async (request) => {
+        const { id } = request.params;
+
+        return app.pg.transact(async (client) => {
+            const deleteLink = await client.query(`
+                DELETE FROM public.job_websites
+                WHERE websiteid = ${id}
+            `);
+            return deleteLink;
+        });
     });
 
     done();
